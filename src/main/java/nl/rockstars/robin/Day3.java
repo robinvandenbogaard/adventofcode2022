12c5733e03e65773e52f3472536cb12191650564
Robin <rjd.vandenbogaard@gmail.com>
2022-12-03 09:35:39 +0100

    Day 3 step 1

--- src/main/java/nl/rockstars/robin/Day3.java
+++ src/main/java/nl/rockstars/robin/Day3.java
@@ -1,7 +1,10 @@
 package nl.rockstars.robin;
 
+import java.util.List;
+
 public class Day3 implements DayProcessor
 {
+    private String priority = "";
 
     public static void main(String[] args )
     {
@@ -10,10 +13,53 @@ public class Day3 implements DayProcessor
 
     @Override
     public void process(String line) {
+        var rucksack = Rucksack.of(line);
+        priority += rucksack.getCommonLetter();
     }
 
     @Override
     public Result getResult() {
-        return new Result("");
+        char[] xx = priority.toCharArray();
+        int sum = 0;
+        for (char x : xx) {
+            String VALUES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            sum += VALUES.indexOf(x)+1;
+        }
+
+        return new Result(sum+"");
     }
+
+
+    record Rucksack(List<Compartment> compartments) {
+        final static int COMPARTMENTS = 2;
+
+        public static Rucksack of(String line) {
+            var middle = line.length()/ COMPARTMENTS;
+            var compartment1 = line.substring(0,middle);
+            var compartment2 = line.substring(middle);
+            return new Rucksack(List.of(new Compartment(compartment1), new Compartment(compartment2)));
+        }
+
+        public String getCommonLetter() {
+            return compartments.stream().reduce(Compartment.BLANK, Compartment::findMatching).items;
+        }
+    }
+
+    record Compartment(String items) {
+        static Compartment BLANK = new Compartment(null);
+
+        public Compartment findMatching(Compartment other) {
+            if (this == BLANK)
+                return other;
+
+            for (var character : other.items.toCharArray()) {
+                if (this.items.contains(String.valueOf(character)))
+                    return new Compartment(String.valueOf(character));
+            }
+
+            throw new IllegalStateException("No match found");
+        }
+
+    }
+
 }
--- src/test/java/nl/rockstars/robin/Day3Test.java
+++ src/test/java/nl/rockstars/robin/Day3Test.java
@@ -1,15 +1,39 @@
 package nl.rockstars.robin;
 
+import nl.rockstars.robin.Day3.Compartment;
 import org.junit.jupiter.api.Test;
 
+import static nl.rockstars.robin.Day3.Rucksack;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 
-public class Day3Test
-{
+public class Day3Test {
     @Test
     void inputTest() {
         var result = new Day3().go().output();
-        assertThat(result, is(""));
+        assertThat(result, is("157"));
+    }
+
+    @Test
+    void rucksack() {
+        var rucksack = Rucksack.of("vJrwpWtwJgWrhcsFMMfFFhFp");
+        assertThat(rucksack.compartments(), contains(
+                new Compartment("vJrwpWtwJgWr"),
+                new Compartment("hcsFMMfFFhFp")));
+
+        var rucksack2 = Rucksack.of("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
+        assertThat(rucksack2.compartments(), contains(
+                new Compartment("jqHRNqRjqzjGDLGL"),
+                new Compartment("rsFMfFZSrLrFZsSL")));
+    }
+
+    @Test
+    void rucksackCommonItem() {
+        var rucksack = Rucksack.of("vJrwpWtwJgWrhcsFMMfFFhFp");
+        assertThat(rucksack.getCommonLetter(), is("p"));
+
+        var rucksack2 = Rucksack.of("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
+        assertThat(rucksack2.getCommonLetter(), is("L"));
     }
 }
