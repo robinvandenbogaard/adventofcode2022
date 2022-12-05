e84b35f8d63a31e615bb5db2dab9694bb25a9cd8
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2022-12-05 10:35:42 +0100

    Day 5 Step 1

--- src/main/java/nl/rockstars/robin/Day5.java
+++ src/main/java/nl/rockstars/robin/Day5.java
@@ -1,18 +1,89 @@
 package nl.rockstars.robin;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Day5 implements DayProcessor {
+
+    private boolean parseStacks = true;
+    private Stacks stacks;
 
     public static void main(String[] args) {
         new Day5().go();
     }
 
     @Override
-    public void process(String line) {
+    public void beforeInput() {
+        this.stacks = new Stacks();
+    }
 
+    @Override
+    public void process(String line) {
+        if (parseStacks) {
+            if (line.isEmpty()) {
+                parseStacks = false;
+                stacks.reverse();
+                return;
+            }
+
+            stacks.addAll(Crate.of(line));
+        } else {
+            stacks.execute(Instruction.of(line));
+        }
     }
 
     @Override
     public Result getResult() {
-        return Result.EMPTY;
+        return new Result(stacks.getTops());
+    }
+
+    record Instruction(int amount, int from, int to) {
+        public static Instruction of(String line) {
+            var tokens = line.split(" ");
+            return new Instruction(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[5]));
+        }
+    }
+    record Crate(int stackNumber, String content) {
+        public static List<Crate> of(String line) {
+            List<Crate> result = new ArrayList<>();
+            var nr = 1;
+            var cursor = 1;
+            while (cursor < line.length()) {
+               var token = String.valueOf(line.charAt(cursor));
+               if (!" ".equals(token)) {
+                   result.add(new Crate(nr, token));
+               }
+               cursor+=4;
+               nr++;
+            }
+            return result;
+        }
+    }
+
+    static class Stacks {
+
+        Map<Integer, Stack<Crate>> stacks = new HashMap<>();
+
+        public void execute(Instruction instruction) {
+            for (int crateNr = 0; crateNr < instruction.amount; crateNr++) {
+                var crate = stacks.get(instruction.from()).pop();
+                stacks.get(instruction.to()).push(crate);
+            }
+        }
+
+        public void addAll(List<Crate> crates) {
+            crates.forEach(crateToAdd-> {
+                var stackToUpdate = stacks.computeIfAbsent(crateToAdd.stackNumber(), (nr)->new Stack<>());
+                stackToUpdate.push(crateToAdd);
+            });
+        }
+
+        public void reverse() {
+            stacks.values().forEach(Collections::reverse);
+        }
+
+        public String getTops() {
+            return stacks.values().stream().map(crates -> crates.isEmpty() ? new Crate(0, " ") : crates.peek()).map(Crate::content).collect(Collectors.joining());
+        }
     }
 }
--- src/test/java/nl/rockstars/robin/Day5Test.java
+++ src/test/java/nl/rockstars/robin/Day5Test.java
@@ -3,7 +3,11 @@ package nl.rockstars.robin;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static nl.rockstars.robin.Day5.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 
 public class Day5Test {
@@ -18,6 +22,50 @@ public class Day5Test {
     @Test
     void inputTest() {
         var result = day5.go().output();
-        assertThat(result, is(""));
+        assertThat(result, is("CMZ"));
     }
+
+    @Test
+    void parseInstruction() {
+        assertThat(Instruction.of("move 2 from 1 to 7"), is(new Instruction(2, 1, 7)));
+        assertThat(Instruction.of("move 1 from 5 to 2"), is(new Instruction(1, 5, 2)));
+    }
+
+    @Test
+    void parseStacks() {
+        assertThat(Crate.of("    [D]"), contains(new Crate(2, "D")));
+        assertThat(Crate.of("[N] [C]"), contains(new Crate(1, "N"), new Crate(2, "C")));
+        assertThat(Crate.of("[Z] [M] [P]"), contains(new Crate(1, "Z"),new Crate(2, "M"),new Crate(3, "P")));
+    }
+
+    @Test
+    void mergeStacks() {
+        var stack = new Stacks();
+        stack.addAll(List.of(new Crate(1, "Z"),new Crate(2, "M"),new Crate(3, "P"), new Crate(1, "X")));
+        assertThat(stack.stacks.get(1), contains(new Crate(1, "Z"), new Crate(1, "X")));
+        assertThat(stack.stacks.get(2), contains(new Crate(2, "M")));
+        assertThat(stack.stacks.get(3), contains(new Crate(3, "P")));
+
+        stack.reverse();
+        assertThat(stack.stacks.get(1), contains(new Crate(1, "X"), new Crate(1, "Z")));
+        assertThat(stack.stacks.get(2), contains(new Crate(2, "M")));
+        assertThat(stack.stacks.get(3), contains(new Crate(3, "P")));
+    }
+    @Test
+    void tops() {
+        var stack = new Stacks();
+        stack.addAll(List.of(new Crate(1, "X")));
+        assertThat(stack.getTops(), is("X"));
+
+        stack.addAll(List.of(new Crate(2, "Z")));
+        assertThat(stack.getTops(), is("XZ"));
+
+        stack.addAll(List.of(new Crate(3, "A")));
+        stack.execute(new Instruction(1, 2,1));
+        assertThat(stack.getTops(), is("Z A"));
+
+    }
+
+
+
 }
