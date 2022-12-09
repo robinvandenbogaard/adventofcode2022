f56aab151ccadb15597fbc47959b9b6178bd01fb
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2022-12-09 19:39:15 +0100

    Day 8 Step 1

--- src/main/java/nl/rockstars/robin/Day8.java
+++ src/main/java/nl/rockstars/robin/Day8.java
@@ -1,17 +1,89 @@
 package nl.rockstars.robin;
 
+import java.util.Arrays;
+
 public class Day8 implements DayProcessor {
+
+    Integer[][] grid;
+    int size;
+    private int currrow;
+    private int total;
 
     public static void main(String[] args) {
         new Day8().go();
     }
 
     @Override
+    public void beforeInput() {
+        grid = new Integer[100][100];
+        currrow = 0;
+    }
+
+    @Override
     public void process(String line) {
+        size = line.length();
+        grid[currrow] = Arrays.asList(line.split("")).stream().map(Integer::parseInt).toList().toArray(new Integer[100]);
+        currrow++;
+    }
+
+    @Override
+    public void afterInput() {
+        total += getEdgeTreeCount(size);
+        total += visibleInnerTrees(grid, size);
+    }
+
+    int getEdgeTreeCount(int size) {
+        return (size * 4) - 4;
+    }
+
+    int visibleInnerTrees(Integer[][] grid, int size) {
+        var result = 0;
+        for (int x = 1; x < size - 1; x++) {
+            for (int y = 1; y < size - 1; y++) {
+                result += Math.min(1,testH(x, y, grid, size)+testV(x, y, grid, size));
+            }
+        }
+        return result;
+    }
+
+    int testH(int x, int y, Integer[][] grid, int size) {
+        if (x == size-1 || y == size-1 || x <= 0 || y <= 0)
+            throw new IllegalStateException("do not check edges");
+
+        var height = grid[x][y];
+        var blockedLeft = false;
+        var blockedRight = false;
+        for (int i = 0; i < size && (!blockedLeft || !blockedRight); i++) {
+            if (x == i)
+                continue;
+            if (i < x)
+                blockedLeft = grid[i][y] >= height || blockedLeft;
+            if (i > x)
+                blockedRight = grid[i][y] >= height || blockedRight;
+        }
+        return blockedLeft && blockedRight ? 0 : 1;
+    }
+
+    int testV(int x, int y, Integer[][] grid, int size) {
+        if (x == size-1 || y == size-1 || x <= 0 || y <= 0)
+            throw new IllegalStateException("do not check edges");
+
+        var height = grid[x][y];
+        var blockedLeft = false;
+        var blockedRight = false;
+        for (int i = 0; i < size && (!blockedLeft || !blockedRight); i++) {
+            if (y == i)
+                continue;
+            if (i < y)
+                blockedLeft = grid[x][i] >= height || blockedLeft;
+            if (i > y)
+                blockedRight = grid[x][i] >= height || blockedRight;
+        }
+        return blockedLeft && blockedRight ? 0 : 1;
     }
 
     @Override
     public Result getResult() {
-        return new Result("");
+        return new Result(total);
     }
 }
--- src/test/java/nl/rockstars/robin/Day8Test.java
+++ src/test/java/nl/rockstars/robin/Day8Test.java
@@ -18,6 +18,135 @@ public class Day8Test {
     @Test
     void inputTest() {
         var result = day8.go().output();
-        assertThat(result, is(""));
+        assertThat(result, is("21"));
+    }
+
+    @Test
+    void edges() {
+        assertThat(day8.getEdgeTreeCount(5), is(16));
+        assertThat(day8.getEdgeTreeCount(100), is(396));
+    }
+
+    @Test
+    void visibleHorizontalTrees() {
+        day8.go();
+        assertThat(day8.testH(1,1, day8.grid, day8.size), is(1));
+        assertThat(day8.testH(2,2, day8.grid, day8.size), is(0));
+        assertThat(day8.testH(3,3, day8.grid, day8.size), is(0));
+    }
+
+    @Test
+    void visibleVerticalTrees() {
+        day8.go();
+        assertThat(day8.testV(1,1, day8.grid, day8.size), is(1));
+        assertThat(day8.testV(2,2, day8.grid, day8.size), is(0));
+        assertThat(day8.testV(3,3, day8.grid, day8.size), is(0));
+    }
+
+    @Test
+    void visibility() {
+        //surrounded
+        assertThat(
+                day8.testH(1,1, toGrid("111 101 111", 3), 3),
+                is(0)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("111 101 111", 3), 3),
+                is(0)
+        );
+
+        //towering above all
+        assertThat(
+                day8.testH(1,1, toGrid("000 010 000", 3), 3),
+                is(1)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("000 010 000", 3), 3),
+                is(1)
+        );
+
+        //towering but surrounded all
+        assertThat(
+                day8.testH(1,1, toGrid("22222 20002 20102 20002 22222", 5), 5),
+                is(0)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("22222 20002 20102 20002 22222", 5), 5),
+                is(0)
+        );
+
+        //Only top
+        assertThat(
+                day8.testH(1,1, toGrid("111 010 000", 3), 3),
+                is(1)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("111 010 000", 3), 3),
+                is(1)
+        );
+
+        //Only bottom
+        assertThat(
+                day8.testH(1,1, toGrid("000 010 111", 3), 3),
+                is(1)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("000 010 111", 3), 3),
+                is(1)
+        );
+
+        //Only Left
+        assertThat(
+                day8.testH(1,1, toGrid("100 110 100", 3), 3),
+                is(1)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("100 110 100", 3), 3),
+                is(1)
+        );
+
+        //Only Right
+        assertThat(
+                day8.testH(1,1, toGrid("001 011 001", 3), 3),
+                is(1)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("001 011 001", 3), 3),
+                is(1)
+        );
+
+        //Equally tall
+        assertThat(
+                day8.testH(1,1, toGrid("000 000 000", 3), 3),
+                is(0)
+        );
+        assertThat(
+                day8.testV(1,1, toGrid("000 000 000", 3), 3),
+                is(0)
+        );
+    }
+
+    @Test
+    void multi() {
+        assertThat(
+                day8.visibleInnerTrees(toGrid("0000 0120 0210 0000", 4), 4),
+                is(4)
+        );
+        assertThat(
+                day8.visibleInnerTrees(toGrid("0000 0120 0210 0000", 4), 4),
+                is(4)
+        );
+    }
+
+    private Integer[][] toGrid(String input, int size) {
+        var grid = new Integer[size][size];
+        var lines = input.split(" ");
+        for (int y = 0; y < lines.length; y++) {
+            var line = lines[y];
+            for (int x = 0; x < line.length(); x++) {
+                grid[x][y] = Integer.parseInt(String.valueOf(line.charAt(x)));
+            }
+        }
+        return grid;
     }
 }
