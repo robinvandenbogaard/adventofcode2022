0a0c90d39c2179f7f6246d7c626f97ff3fd7414f
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2022-12-15 20:37:28 +0100

    Prep Day 14

---
+++ src/main/java/nl/rockstars/robin/Day14.java
@@ -0,0 +1,13 @@
+package nl.rockstars.robin;
+
+public class Day14 implements DayProcessor {
+    @Override
+    public void process(String line) {
+
+    }
+
+    @Override
+    public Result getResult() {
+        return new Result("");
+    }
+}
---
+++ src/test/java/nl/rockstars/robin/Day14Test.java
@@ -0,0 +1,23 @@
+package nl.rockstars.robin;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class Day14Test {
+
+    private Day14 day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day14();
+    }
+
+    @Test
+    void getResult() {
+        var result = day.go().output();
+        assertThat(result, is(""));
+    }
+}
