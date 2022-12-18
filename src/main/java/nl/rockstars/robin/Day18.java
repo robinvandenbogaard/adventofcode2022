d10744d5c249093e198a1caeb52d7faceb43ffd1
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2022-12-18 22:22:54 +0100

    Day 18 Step 2

--- src/main/java/nl/rockstars/robin/Day18.java
+++ src/main/java/nl/rockstars/robin/Day18.java
@@ -2,50 +2,171 @@ package nl.rockstars.robin;
 
 import nl.rockstars.robin.util.Point3D;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class Day18 implements DayProcessor {
 
+    private final Box world;
 
-    public static void main(String[] args) {
-        new Day18().go();
+    public Day18(int worldSize) {
+        this.world = new Box(worldSize);
     }
 
-    private final List<Cube> cubes = new ArrayList<>();
+    public static void main(String[] args) {
+        new Day18(25).go();
+    }
 
     @Override
     public void process(String line) {
         var split = line.split(",");
-        var point = Cube.of(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2]));
-        cubes.add(point);
+        int x = Integer.parseInt(split[0]);
+        int y = Integer.parseInt(split[1]);
+        int z = Integer.parseInt(split[2]);
+        var cube = Cube.boundary(x, y, z);
+
+        world.add(cube);
     }
 
     @Override
     public Result getResult() {
-        var result = cubes.stream().mapToInt(c->c.countUncovered(cubes)).sum();
-        return new Result(result);
+        flood(Point3D.of(0,0,0));
+
+        var lavaBlocks = world.getAll(CubeType.Lava);
+        var sum = lavaBlocks.stream().mapToInt(c ->
+                c.countUncovered(world)).sum();
+
+        return new Result(sum);
     }
 
-    record Cube(Point3D p) {
+    private void flood(Point3D start) {
+        var queue = new ArrayDeque<Point3D>();
+        queue.add(start);
+        var flooded = 0;
+        while (!queue.isEmpty()) {
+            var p = queue.pop();
+            var type = world.get(p);
+            if (type == CubeType.Empty) {
+                var cube = Cube.steam(p);
+                world.add(cube);
+                flooded++;
+                queue.addAll(cube.neighbours());
+            }
+        }
+        System.out.println("flooded "+flooded+" cubes out of "+(7*7*7));
+    }
 
-        public static Cube of(int x, int y, int z) {
-            return new Cube(Point3D.of(x,y,z));
+    record Cube(Point3D p, CubeType type) {
+
+        public static Cube boundary(int x, int y, int z) {
+            return new Cube(Point3D.of(x, y, z), CubeType.Lava);
         }
 
-        public static Cube of(Point3D p) {
-            return new Cube(p);
+        public static Cube empty(Point3D p) {
+            return new Cube(p, CubeType.Empty);
         }
 
+        public static Cube steam(int x, int y, int z) {
+            return new Cube(Point3D.of(x, y, z), CubeType.Steam);
+        }
 
-        public int countUncovered(List<Cube> cubes) {
-            var n = new ArrayList<>(neighbours());
-            n.removeAll(cubes);
+        public static Cube of(Point3D p, CubeType type) {
+            return new Cube(p, type);
+        }
+
+        public static Cube steam(Point3D p) {
+            return new Cube(p, CubeType.Steam);
+        }
+
+        public static Cube empty(int x, int y, int z) {
+            return Cube.of(Point3D.of(x, y, z), CubeType.Empty);
+        }
+
+        public int countUncovered(Box world) {
+            List<Point3D> n = new ArrayList<>(neighbours());
+            n.removeIf(pos -> {
+                var x = world.get(pos);
+                return x != CubeType.Steam && x!= CubeType.Void;
+            });
             return n.size();
         }
 
-        public List<Cube> neighbours() {
-            return List.of(Cube.of(p.north()), Cube.of(p.south()), Cube.of(p.east()), Cube.of(p.west()), Cube.of(p.up()), Cube.of(p.down()));
+        public List<Point3D> neighbours() {
+            return Stream.of(
+                    p.north(),
+                    p.south(),
+                    p.east(),
+                    p.west(),
+                    p.up(),
+                    p.down()
+            ).toList();
+        }
+
+        public Cube as(CubeType type) {
+            return new Cube(p, type);
+        }
+    }
+
+    enum CubeType {
+        Lava, Steam, Empty, Void
+    }
+
+    public static class Box {
+
+        static int margin = 4;
+
+        static int OFFSET = 2;
+
+        CubeType[][][] world;
+        private final int worldSize;
+
+        public Box(int worldSize) {
+            world = new CubeType[worldSize + margin][worldSize + margin][worldSize + margin];
+            this.worldSize = worldSize;
+            for (int x = 0; x < worldSize+margin; x++) {
+                for (int y = 0; y < worldSize+margin; y++) {
+                    for (int z = 0; z < worldSize+margin; z++) {
+                        world[x][y][z] = CubeType.Empty;
+                    }
+                }
+            }
+        }
+
+        public void forEachWorld(Consumer<Point3D> s) {
+            for (int x = 0; x < worldSize+margin; x++) {
+                for (int y = 0; y < worldSize+margin; y++) {
+                    for (int z = 0; z < worldSize+margin; z++) {
+                        s.accept(Point3D.of(x, y, z));
+                    }
+                }
+            }
+        }
+
+        public List<Cube> getAll(CubeType type) {
+            var result = new ArrayList<Cube>();
+            forEachWorld(point->{
+                CubeType cubeType = get(point);
+                if (cubeType == type)
+                    result.add(Cube.of(point, type));
+            });
+            return result;
+        }
+
+        public void add(Cube cube) {
+            world[cube.p.x() + OFFSET][cube.p.y() + OFFSET][cube.p.z() + OFFSET] = cube.type;
+        }
+
+        public CubeType get(Point3D p) {
+            var x = p.x() + OFFSET;
+            int y = p.y() + OFFSET;
+            int z = p.z() + OFFSET;
+            var bound = worldSize+margin;
+            if (x<0 || y < 0 || z < 0 || x >= bound || y >= bound || z >= bound)
+                return CubeType.Void;
+            return world[x][y][z];
         }
     }
 }
--- src/test/java/nl/rockstars/robin/Day18Test.java
+++ src/test/java/nl/rockstars/robin/Day18Test.java
@@ -1,11 +1,13 @@
 package nl.rockstars.robin;
 
+import nl.rockstars.robin.util.Point3D;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
+import static nl.rockstars.robin.Day18.Box;
 import static nl.rockstars.robin.Day18.Cube;
+import static nl.rockstars.robin.Day18.CubeType.Void;
+import static nl.rockstars.robin.Day18.CubeType.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -15,23 +17,67 @@ class Day18Test {
 
     @BeforeEach
     void setUp() {
-        day = new Day18();
+        day = new Day18(7);
     }
 
     @Test
     void getResult() {
         var result = day.go().output();
-        assertThat(result, is("64"));
+        assertThat(result, is("58"));
+    }
+
+    @Test
+    void getResult2() {
+        var day = new Day18(7) {
+            @Override
+            public String getFile() {
+                return "18-2.txt";
+            }
+        };
+        var result = day.go().output();
+        assertThat(result, is("90"));
     }
 
     @Test
     void notCovered() {
-        assertThat(Cube.of(0,0,0).countUncovered(List.of()), is(6));
+        var world = new Box(6);
+        world.add(Cube.steam(0,0,1));
+        world.add(Cube.steam(0,0,-1));
+        world.add(Cube.steam(0,1,0));
+        world.add(Cube.steam(0,-1,0));
+        world.add(Cube.steam(1,0,0));
+        world.add(Cube.steam(-1,0,0));
+        assertThat(Cube.boundary(0,0,0).countUncovered(world), is(6));
     }
 
     @Test
     void covered() {
-        var cubes = List.of(Cube.of(0,0,1));
-        assertThat(Cube.of(0,0,0).countUncovered(cubes), is(5));
+        var world = new Box(6);
+        world.add(Cube.steam(0,0,1));
+        world.add(Cube.steam(0,0,-1));
+        world.add(Cube.steam(0,1,0));
+        world.add(Cube.steam(0,-1,0));
+        world.add(Cube.steam(1,0,0));
+        world.add(Cube.steam(-1,0,0));
+
+        //empty covers
+        world.add(Cube.empty(1,0,0));
+        assertThat(Cube.boundary(0,0,0).countUncovered(world), is(5));
+
+        //boundary covers
+        world.add(Cube.boundary(-1,0,0));
+        assertThat(Cube.boundary(0,0,0).countUncovered(world), is(4));
+
+        //void does not cover
+        world.add(new Cube(Point3D.of(0,-1,0), Void));
+        assertThat(Cube.boundary(0,0,0).countUncovered(world), is(4));
+    }
+
+    @Test
+    void as() {
+        var cube = Cube.steam(0,0,1);
+        assertThat(cube.as(Steam), is(Cube.steam(0,0,1)));
+        assertThat(cube.as(Lava), is(Cube.boundary(0,0,1)));
+        assertThat(cube.as(Empty), is(Cube.empty(Point3D.of(0,0,1))));
     }
 }
